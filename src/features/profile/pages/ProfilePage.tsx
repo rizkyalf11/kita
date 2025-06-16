2a3de117fce57f6b7a1564c0c89ea51d9d957338
rizky a <107133530+rizkyalf11@users.noreply.github.com>
2025-06-16 17:16:22 +0700

    feat: login (fix 1)

---
+++ src/features/profile/pages/ProfilePage.tsx
@@ -0,0 +1,17 @@
+import { AuthRoute } from '~/components/layout/AuthRoute'
+import { PageContainer } from '~/components/layout/PageContainer'
+import { SectionContainer } from '~/components/layout/SectionContainer'
+
+const ProfilePage = () => {
+  return (
+    <AuthRoute>
+      <PageContainer>
+        <SectionContainer>
+          
+        </SectionContainer>
+      </PageContainer>
+    </AuthRoute>
+  )
+}
+
+export default ProfilePage
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,41 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { PageContainer } from "~/components/layout/PageContainer";
+import { SectionContainer } from "~/components/layout/SectionContainer";
+import { Button } from "~/components/ui/button";
+import { supabase } from "~/lib/supabase/client";
 
 export default function Home() {
+  const [isLogin, setIsLogin] = useState<"loading" | true | false>("loading");
+  const router = useRouter();
+
+  useEffect(() => {
+    void (async function () {
+      const { data } = await supabase.auth.getUser();
+
+      if (data.user) {
+        setIsLogin(true);
+      } else {
+        setIsLogin(false);
+      }
+    })();
+  }, []);
+
+  const handleLogOut = async () => {
+    await supabase.auth.signOut();
+    toast("Anda telah keluar");
+    setIsLogin(false)
+  };
 
   return (
-    <>
-    
-    </>
+    <PageContainer>
+      <SectionContainer>
+        {isLogin && <Button onClick={handleLogOut}>Keluar</Button>}
+        {!isLogin && (
+          <Button onClick={() => router.replace("/login")}>Masuk</Button>
+        )}
+      </SectionContainer>
+    </PageContainer>
   );
 }
