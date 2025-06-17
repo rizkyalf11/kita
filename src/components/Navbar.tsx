25cf4514ff2ca3dfe2e3f9890923d23de2932b7d
rizky a <107133530+rizkyalf11@users.noreply.github.com>
2025-06-17 09:54:15 +0700

    ui: navbar (fix 1)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,24 +12,24 @@ const Navbar = () => {
   const { setTheme } = useTheme();
 
   return (
-    <div className="bg-card text-card-foreground fixed right-1/2 bottom-10 flex translate-x-1/2 items-center gap-1 rounded-xl border px-2 py-2 shadow-sm">
-      <Button size="icon" className="size-12" variant="ghost">
+    <div className="bg-card text-card-foreground fixed right-1/2 bottom-3 flex translate-x-1/2 items-center gap-1 rounded-xl border px-2 py-2 shadow-sm lg:bottom-10">
+      <Button size="icon" className="size-10 lg:size-12" variant="ghost">
         <Home />
       </Button>
-      <Button size="icon" className="size-12" variant="ghost">
+      <Button size="icon" className="size-10 lg:size-12" variant="ghost">
         <Search />
       </Button>
-      <Button size="icon" className="size-12" variant="ghost">
+      <Button size="icon" className="size-10 lg:size-12" variant="ghost">
         <SquarePlus />
       </Button>
-      <Button size="icon" className="size-12" variant="ghost">
+      <Button size="icon" className="size-10 lg:size-12" variant="ghost">
         <User />
       </Button>
       <div className="bg-border h-[30px] w-[1.5px]" />
 
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant="ghost" size="icon" className="size-12">
+          <Button variant="ghost" size="icon" className="size-10 lg:size-12">
             <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
             <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
             <span className="sr-only">Toggle theme</span>
